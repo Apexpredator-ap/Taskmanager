8970e928ab81ce1c4398a96d673d647fb62ec96a
ABHI <abhiramm95@gmail.com>
2024-10-06 19:07:01 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:taskmanager/view/splash.dart';
 
+
 void main(){
   runApp(MyApp());
 }
